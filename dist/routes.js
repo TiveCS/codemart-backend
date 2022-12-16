fc962247b742cd677b787a46a1c6f9281c1fdc2f
TiveCS <ahmadfathanhanif@gmail.com>
2022-12-16 15:10:39 +0700

    ignore dist

--- dist/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const router_1 = require("./auth/router");
-exports.router = (0, express_1.Router)();
-exports.router.use('/auth', router_1.AuthRouter);
